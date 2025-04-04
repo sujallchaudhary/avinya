17cfa8497afb77c52e99b4543ea12d89f4d9c5ac
sujal <me@sujal.info>
2025-04-04 17:39:47 +0530

    image opt

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'sdrive.blr1.cdn.digitaloceanspaces.com',
-
-      },
-    ],
+    unoptimized: true,
   },
 };
 
